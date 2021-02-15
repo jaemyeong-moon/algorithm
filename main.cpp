68d6b26c839bbbb485b0d6b2e0323099734490c1
jaemyeong Moon <answoaud11@naver.com>
2021-02-15 21:38:01 +0900

    미로탐색

--- main.cpp
+++ main.cpp
@@ -1,65 +1,98 @@
 #include "cstring"
 #include "iostream"
 #include "stdlib.h"
-
+#include "queue"
 using namespace std;
 /*
-최대 공약수와 최소공배수 
+N×M크기의 배열로 표현되는 미로가 있다.
 
+1	0	1	1	1	1
+1	0	1	0	1	0
+1	0	1	0	1	1
+1	1	1	0	1	1
+미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
 
-유클리드 호제법 예)
-1071과 1029의 최대공약수를 구하면,
-1071은 1029로 나누어떨어지지 않기 때문에, 1071을 1029로 나눈 나머지를 구한다. ≫ 42
-1029는 42로 나누어떨어지지 않기 때문에, 1029를 42로 나눈 나머지를 구한다. ≫ 21
-42는 21로 나누어떨어진다.
-따라서, 최대공약수는 21이다.
+위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
 
+입력
+첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
 
+출력
+첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
 
-https://www.acmicpc.net/problem/2609
+예제 입력 1 
+4 6
+101111
+101010
+101011
+111011
+예제 출력 1 
+15
 
+https://www.acmicpc.net/problem/2178
 
-
-
+예전에 자바로 제출한적 있음. 
 */
+int a=0, b=0;
+int arr[100][100];
+bool tracker[100][100];
+int X[]= {1,0,-1,0};
+int Y[]= {0,1,0,-1};
 
-int uclid(int a, int b)
+struct point
 {
-    int v1,v2,remain;
+    int x;
+    int y;
+    int depth;
+};
 
-    if(a > b)
-    {
-        v1 = a;
-        v2 = b;
-    } 
-    else
-    {
-        v1 = b;
-        v2 = a;
-    } 
-    if( (v1==0 || v2 == 0) ) return -1;   
-    do
-    {      
-      remain = v1%v2;
-      v1 = v2;
-      v2 = remain;
-    }    
-    while(remain != 0);
-    
-    return v1;
+struct point p;
 
+struct point Point(int x,int y, int d)
+{
+    p.x =x;
+    p.y = y;
+    p.depth = d;
+    return p;
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
-    int a=1,b=1,re1=1,re2=1;
     scanf("%d %d", &a, &b);
-    re1 = uclid(a,b);
-    if(re1 > 0 ) re2 = a*b/re1;
-    cout << re1 <<'\n';
-    cout << re2 <<'\n';
+    queue<struct point> q;
+    p.x = p.y = p.depth = 0;  
+    for(int i = 0; i < a; i++)
+    {   char str[101];             
+        scanf("%s",str);       
+        for(int z = 0; z < b ; z++)
+        {
+            arr[i][z] = str[z] - '0';        
+            tracker[i][z] = false;
+        }
+    }   
+    q.push(Point(0,0,1));     
+    int prv_x,prv_y;
+    while(!q.empty())
+    {
+        struct point p = q.front();
+		q.pop();
+        if(p.x == a-1 && p.y == b-1)
+        {
+            printf("%d\n",p.depth);
+			break;
+        }
+        for(int i = 0; i < 4; i++) //4방향으로 갈수있는 경우를 게산.
+        {    
+            int x = p.x+X[i];
+            int y = p.y+Y[i];        
+            if(x>=0 && x<a && y >=0 && y <b && tracker[x][y] == false && arr[x][y] == 1 )
+            {                
+                q.push(Point(x,y,p.depth+1));  
+                tracker[x][y] = true;
+            }           
+        }
+    }
     return 0 ;
 }
-
-
