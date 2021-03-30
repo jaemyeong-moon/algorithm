d3c48903d6ae6d1a47e99d63050039e8467673d9
jaemyeong Moon <answoaud11@naver.com>
2021-03-31 01:02:48 +0900

    baekjoon 1850 / 최대공약수 문제

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 #include "stdlib.h"
 #include "queue"
 using namespace std;
+typedef unsigned long long UINT64;
+#define MAX(a,b) a>b ? a : b
+#define MIN(a,b) a<b ? a : b
+
 /*
 
 https://www.acmicpc.net/problem/15953
 
 카카오 페스티벌 예선 문제 1
 */
-int _2017[100] = {0,5000000,3000000,2000000,500000,300000,100000};
-int _2018[100] = {0,5120000,2560000,1280000,640000,320000};
-int result[1000] = {0,};
-int a,b;
-int pow(int a, int b)
+
+UINT64 a,b;
+int pow(UINT64 a, UINT64 b)
 {
     if(b == 0) return 1;
     return a*pow(a,b-1);
@@ -21,37 +23,32 @@ int pow(int a, int b)
 int main()
 {   
     ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
-    int T = 0;;
-    scanf("%d",&T);
-    if(T < 0 || T > 1000) return -1;
-    for(int i = 0 ; i < T ; i++)
+    cin.tie(NULL);     
+    scanf("%lld %lld", &a, &b);
+    
+
+    UINT64 max = MAX(a,b);
+    UINT64 min = MIN(a,b);
+    UINT64 temp;
+    
+    while( (max = max % min) != 0)
     {
-        scanf("%d %d", &a, &b);
-        if(a < 1 || a > 100 ) a = 0 ;
-        else if( b < 1 || b > 64) b = 0;
-        int rank1 = 0, rank2 = 0;
-        
-        for(int j = 1 ; a > 0 ; j++)
+        if(max < min)
         {
-            if( (a = a - j) < 1 ) 
-            {
-                rank1 = j;
-                break;
-            }
+            temp = max;
+            max = min;
+            min = temp;
         }
-        for(int j = 1 ; b > 0 ; j++)
-        {
-            if( (b = b - pow(2,j-1)) < 1 )
-            {
-                rank2 = j;
-                break;
-            } 
-        }        
-        result[i] = _2017[rank1]+_2018[rank2];
     }
-    for(int i = 0 ; i < T; i++)
+
+    if( min > 10000000) min = 10000000;
+    for(int i = 0 ; i < min ; i++)
     {
-       cout << result[i] << '\n';
+        printf("1");
     }
+    
+
+
+
+     
 }
