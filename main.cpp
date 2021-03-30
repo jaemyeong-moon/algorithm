0bf509f9f0719da3e83fd9b85a5e6ca182f0adf7
jaemyeong Moon <answoaud11@naver.com>
2021-03-30 21:40:37 +0900

    baekjoon 2839

--- main.cpp
+++ main.cpp
@@ -4,95 +4,46 @@
 #include "queue"
 using namespace std;
 /*
-N×M크기의 배열로 표현되는 미로가 있다.
 
-1	0	1	1	1	1
-1	0	1	0	1	0
-1	0	1	0	1	1
-1	1	1	0	1	1
-미로에서 1은 이동할 수 있는 칸을 나타내고, 0은 이동할 수 없는 칸을 나타낸다. 이러한 미로가 주어졌을 때, (1, 1)에서 출발하여 (N, M)의 위치로 이동할 때 지나야 하는 최소의 칸 수를 구하는 프로그램을 작성하시오. 한 칸에서 다른 칸으로 이동할 때, 서로 인접한 칸으로만 이동할 수 있다.
+https://www.acmicpc.net/problem/2839
 
-위의 예에서는 15칸을 지나야 (N, M)의 위치로 이동할 수 있다. 칸을 셀 때에는 시작 위치와 도착 위치도 포함한다.
+상근이는 요즘 설탕공장에서 설탕을 배달하고 있다. 상근이는 지금 사탕가게에 설탕을 정확하게 N킬로그램을 배달해야 한다. 설탕공장에서 만드는 설탕은 봉지에 담겨져 있다. 봉지는 3킬로그램 봉지와 5킬로그램 봉지가 있다.
 
-입력
-첫째 줄에 두 정수 N, M(2 ≤ N, M ≤ 100)이 주어진다. 다음 N개의 줄에는 M개의 정수로 미로가 주어진다. 각각의 수들은 붙어서 입력으로 주어진다.
+상근이는 귀찮기 때문에, 최대한 적은 봉지를 들고 가려고 한다. 예를 들어, 18킬로그램 설탕을 배달해야 할 때, 3킬로그램 봉지 6개를 가져가도 되지만, 5킬로그램 3개와 3킬로그램 1개를 배달하면, 더 적은 개수의 봉지를 배달할 수 있다.
 
-출력
-첫째 줄에 지나야 하는 최소의 칸 수를 출력한다. 항상 도착위치로 이동할 수 있는 경우만 입력으로 주어진다.
-
-예제 입력 1 
-4 6
-101111
-101010
-101011
-111011
-예제 출력 1 
-15
-
-https://www.acmicpc.net/problem/2178
-
-예전에 자바로 제출한적 있음. 
+상근이가 설탕을 정확하게 N킬로그램 배달해야 할 때, 봉지 몇 개를 가져가면 되는지 그 수를 구하는 프로그램을 작성하시오.
 */
-int a=0, b=0;
-int arr[100][100];
-bool tracker[100][100];
-int X[]= {1,0,-1,0};
-int Y[]= {0,1,0,-1};
+int a;
 
-struct point
+int calculate(int v)
 {
-    int x;
-    int y;
-    int depth;
-};
+    if(v < 3 || v > 5000) {
+       
+       return -1;
+   }
+   //3a + 5b = v
+   //b가 가장 큰 경우를 구하라.
+   int start = v / 5;
+   int left;
+   int i; // 5키로짜리 갯수
+   for(i = start ; i > -1 ; i-- )
+   {
+       left = v - 5*i;
+       if(left % 3 == 0 ) break;
+   }
+   if(i < 0 ) return i;
+   return i + (left/3);
 
-struct point p;
 
-struct point Point(int x,int y, int d)
-{
-    p.x =x;
-    p.y = y;
-    p.depth = d;
-    return p;
 }
-
 int main()
-{
+{   
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL); 
-    scanf("%d %d", &a, &b);
-    queue<struct point> q;
-    p.x = p.y = p.depth = 0;  
-    for(int i = 0; i < a; i++)
-    {   char str[101];             
-        scanf("%s",str);       
-        for(int z = 0; z < b ; z++)
-        {
-            arr[i][z] = str[z] - '0';        
-            tracker[i][z] = false;
-        }
-    }   
-    q.push(Point(0,0,1));     
-    int prv_x,prv_y;
-    while(!q.empty())
-    {
-        struct point p = q.front();
-		q.pop();
-        if(p.x == a-1 && p.y == b-1)
-        {
-            printf("%d\n",p.depth);
-			break;
-        }
-        for(int i = 0; i < 4; i++) //4방향으로 갈수있는 경우를 게산.
-        {    
-            int x = p.x+X[i];
-            int y = p.y+Y[i];        
-            if(x>=0 && x<a && y >=0 && y <b && tracker[x][y] == false && arr[x][y] == 1 )
-            {                
-                q.push(Point(x,y,p.depth+1));  
-                tracker[x][y] = true;
-            }           
-        }
-    }
-    return 0 ;
+    scanf("%d", &a);
+    int re = calculate(a);
+    
+    cout << re << endl;
+    
+
 }
