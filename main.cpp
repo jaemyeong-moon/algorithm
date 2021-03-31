8b4050c3079eea9db48b63726fe0db4347a2f426
jaemyeong Moon <answoaud11@naver.com>
2021-03-31 23:04:48 +0900

    순열 사이클. https://www.acmicpc.net/problem/10451

--- main.cpp
+++ main.cpp
@@ -3,52 +3,55 @@
 #include "stdlib.h"
 #include "queue"
 using namespace std;
-typedef unsigned long long UINT64;
-#define MAX(a,b) a>b ? a : b
-#define MIN(a,b) a<b ? a : b
 
 /*
 
-https://www.acmicpc.net/problem/15953
+https://www.acmicpc.net/problem/10451
 
-카카오 페스티벌 예선 문제 1
+순열 사이클
 */
+#include <iostream>
 
-UINT64 a,b;
-int pow(UINT64 a, UINT64 b)
+#include <cstring>
+
+using namespace std;
+
+const int MAX = 1000 + 1;
+
+int N;
+int arr[MAX];
+bool visited[MAX];
+void DFS(int node)
 {
-    if(b == 0) return 1;
-    return a*pow(a,b-1);
+    visited[node] = true;
+    if (!visited[arr[node]])
+        DFS(arr[node]);
 }
-int main()
-{   
-    ios_base::sync_with_stdio(false); 
-    cin.tie(NULL);     
-    scanf("%lld %lld", &a, &b);
-    
 
-    UINT64 max = MAX(a,b);
-    UINT64 min = MIN(a,b);
-    UINT64 temp;
-    
-    while( (max = max % min) != 0)
+int main(void)
+{
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for (int t = 0; t < T; t++)
     {
-        if(max < min)
-        {
-            temp = max;
-            max = min;
-            min = temp;
-        }
+        memset(visited, false, sizeof(visited));
+        cin >> N;
+
+        for (int i = 1; i <= N; i++)
+            cin >> arr[i];
+        int cnt = 0;
+        for (int i = 1; i <= N; i++)
+            //컴포넌트 개수
+            if (!visited[i])
+            {
+                DFS(i);
+                cnt++;
+            }
+        cout << cnt << "\n";
     }
 
-    if( min > 10000000) min = 10000000;
-    for(int i = 0 ; i < min ; i++)
-    {
-        printf("1");
-    }
-    
-
-
-
-     
+    return 0;
 }
