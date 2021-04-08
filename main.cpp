e8b139c5e2ec119b47699493a4985ea25f61510e
jaemyeong Moon <answoaud11@naver.com>
2021-04-09 00:01:57 +0900

    kakao - 순위검색 - 그룹화

--- main.cpp
+++ main.cpp
@@ -1,85 +1,125 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <string>
 #include <vector>
-#include <queue>
-#include <functional>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 /*
-https://www.acmicpc.net/problem/1202
+https://programmers.co.kr/learn/courses/30/lessons/72412
 
-보석넣기 문제
-
-priority_queue 사용 
-
-priority_queue 는 기본적으로 오름차순.
-priority_queue<int, vector<int>, (비교함수)greater<int> > 를쓰면 오름차순/내림차순 설정가능 
-
-sort 사용
-
-sort(int*, int* + N) = 포인터부터 N개만큼 정렬
-sort(vector.begin(), vector.begin() + N) = 포인터부터 N개만큼 정렬
-
-
-
-sort(vector.begin(), vector.end()) 
-sort(vector.begin(), vector.end(), 함수를 만들어서 넣는다. 앞에 인자에 대하여 ) 
-int A[100]
-ex) sort(A,A+100,desc)
-bool desc(int a, int b){ return a > b ;} 내림차순 시.
-
-
-greater<>()
-
-
+카카오 - 순위검색.
 
 */
+vector<int> P[3][2][2][2]; //그룹화하는 벡터.
 
-int N,K;
-int bag[300000]; //각 가방 무게
-// int jewelry[300000][2]; // M / V 
+int getValue(string str)
+{
+    if(str == "cpp") return 0;
+    if(str == "java") return 1;
+    if(str == "python") return 2;
+    if(str == "backend") return 0;
+    if(str == "frontend") return 1;
+    if(str == "junior") return 0;
+    if(str == "senior") return 1;
+    if(str == "chicken") return 0;
+    if(str == "pizza") return 1;
+    return -1;    
+}
+int get(vector<int> list, int score)
+{
+    int idx = lower_bound(list.begin(),list.end(),score) - list.begin();
+    if(idx == list.size()) return 0;
+    return list.size() - idx;       
+}
+int getResult(string query)
+{
+    vector<int> q;
+    string str = "";
+    for (int j = 0; j < query.length(); j++)
+    {
+        if (query[j] == ' ')
+        {
+            if(str != "and")
+                q.push_back(getValue(str));
+            str = "";            
+        }
+        else
+            str += query[j];
+    }
+    int score = atoi(str.c_str());
+    q.push_back(score);
 
-vector<pair<int, int> > jewelry;
-priority_queue<int> pq;
+    int ret = 0;
+    for (int a = 0; a < 3; a++)
+    {
+        if (q[0] != -1 && q[0] != a)
+            continue; //언어에 대하여, -1이면 a값을 모두 탐색한다. -1이 아니면, 맞는 언어만.
+        for (int b = 0; b < 2; b++)
+        {
+            if (q[1] != -1 && q[1] != b)
+                continue;
+            for (int c = 0; c < 2; c++)
+            {
+                if (q[2] != -1 && q[2] != c)
+                    continue;
+                for (int d = 0; d < 2; d++)
+                {
+                    if (q[3] != -1 && q[3] != d)
+                        continue;
+                    ret += get(P[a][b][c][d], q[4]);              
+                }
+            }
+        }        
+    }
+    return ret;
+}
+
+
+vector<int> solution(vector<string> info, vector<string> query) {
+    vector<int> answer;
+    for(int i = 0 ; i < info.size() ; i++)
+    {
+        string str = "";
+        vector<string> arr;
+        for(int j = 0; j < info[i].length(); j++)
+        {
+            if(info[i][j] == ' ')
+            {            
+                arr.push_back(str);
+                str = "";
+            }
+            else str+=info[i][j];
+        }
+        int it[4];
+        for(int j = 0; j < arr.size() ; j++)
+        {
+            it[j] = getValue(arr[j]);
+        }
+        int score = atoi(str.c_str());
+        
+        P[it[0]][it[1]][it[2]][it[3]].push_back(score);        
+    }
+    for (int a = 0; a < 3; a++)
+     for (int b = 0; b < 2; b++)
+            for (int c = 0; c < 2; c++)
+                for (int d = 0; d < 2; d++)
+                    sort(P[a][b][c][d].begin(), P[a][b][c][d].end());
+
+
+    for(int i = 0 ; i < query.size() ; i++)
+    {
+        int ret = getResult(query[i]);
+        answer.push_back(ret);
+    }
+    
+    return answer;
+}
 int main()
 {
-    cin >> N;
-    cin >> K;
-
-    for(int i = 0 ; i < N ; i++)
-    {
-        int m,v;        
-        // cin >> jewelry[i][0];
-        // cin >> jewelry[i][1];
-        cin >> m;
-        cin >> v;
-        jewelry.push_back(make_pair(m,v));
-    }
-    for(int i = 0 ; i < K; i++)
-        cin >> bag[i];
-
-        
-    // sort(jewelry.begin(),jewelry.end());
-    sort(jewelry.begin(),jewelry.end(), greater<>());
-    
-    sort(bag, bag + K );
-
-    long long sum = 0;
-    int idx = 0;
-    for(int i = 0 ; i < K  ; i++ ) // K 개의 가방에 담아야하고,
-    {
-        // 가방에 담을 수 있는 주얼리를 pq에 넣는다.
-        while( idx < N && jewelry[idx].first <= bag[i] )
-            pq.push(jewelry[idx++].second);
-        if( !pq.empty() ) //pq가 비어있찌 않으면
-        {
-            sum += pq.top();  //가장 비싼놈을 꺼내서 합한다.
-            pq.pop();
-        }        
-    } 
-
-    cout << sum << '\n';
+    vector<string> a = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
+    vector<string> b = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
+    solution(a,b);
 
     return 0;
 }
